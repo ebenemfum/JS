8d41e6fd07b9acfb08cfc3e3e18cb6af69daf6c9
ebenemfum <65869086+ebenemfum@users.noreply.github.com>
2022-06-07 11:49:37 +0000

    Game!!

---
+++ MadLib JS Game/game.js
@@ -0,0 +1,10 @@
+function wordBlacks(myNoun, myAdjective, myVerb, myAdverb) {
+
+    var result ="";
+    result += "The" + " " +myAdjective +" "+ myNoun +" "+ myVerb +" to the store"+" "+myAdverb+"."
+
+    return result
+
+}    
+
+console.log(wordBlacks("dog","big","ran","quickly"))
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello from Java" + "Script") //How to Print a Message
+console.log("Hello from Java" + "Script"); //How to Print a Message
+
+/* 
+This is how you block comment
+*/
+
+//Data Types and Variables
+
+var myName = "Beau is the \"first\" person in the";
+myName[4]="you"
+console.log(myName[4]); 
+
+myName = 8;
+
+console.log(myName); 
+
+let ourName = "Beauus";
+
+const pi= 3.14;
